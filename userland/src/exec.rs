61f0089c0a53000bb92f19c3be436fa9a94f0087
Nicolo Ridulfo <nicoloridulfo@gmail.com>
2024-06-06 23:22:38 +0200

    feat: add parent env to child process

--- userland/src/exec.rs
+++ userland/src/exec.rs
@@ -2,5 +2,9 @@ use std::process::{Command, ExitStatus};
 use std::io::Result;
 
 pub fn execute(exe: &str, args: &[&str]) -> Result<ExitStatus> {
-    Command::new(exe).args(args).spawn()?.wait()
+    Command::new(exe)
+        .args(args)
+        .envs(std::env::vars()) // Share parent's environment with the spawned process
+        .spawn()?
+        .wait()
 }
