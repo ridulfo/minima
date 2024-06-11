db0a303b7605a32f7d314add90aed06bc9e115c1
Nicolo Ridulfo <nicoloridulfo@gmail.com>
2024-06-11 14:53:11 +0200

    feat: add built-in commands to shell

--- userland/src/bin/shell.rs
+++ userland/src/bin/shell.rs
@@ -5,7 +5,8 @@ use userland::exec::execute;
 fn main() {
     loop {
         print!("> ");
-        std::io::stdout().flush().unwrap();
+        std::io::stdout().flush().unwrap(); // Flush the buffer to ensure the prompt is displayed
+
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
         let input = input.trim();
@@ -13,29 +14,34 @@ fn main() {
         let command = parts.next().unwrap(); // Get the first part as the command
         let args: Vec<&str> = parts.collect();
 
-        if command.trim().is_empty() {
-            continue;
-        }
-
-        if command == "exit" {
-            break;
-        }
-
-        if command == "env" {
-            for (key, value) in std::env::vars() {
-                println!("{}={}", key, value);
+        match command.trim() {
+            "" => continue,
+            "cd" => {
+                if args.is_empty() {
+                    eprintln!("Usage: cd <directory>");
+                    continue;
+                }
+                std::env::set_current_dir(args[0]).unwrap();
             }
-            continue;
-        }
-
-        if command == "cd" {
-            std::env::set_current_dir(args[0]).unwrap();
-            continue;
-        }
-
-        match execute(command, &args) {
-            Err(e) => eprintln!("Error executing command: {:?}", e),
-            _ => (),
+            "exit" => break,
+            "env" => {
+                for (key, value) in std::env::vars() {
+                    println!("{}={}", key, value);
+                }
+            }
+            "write" => { // Temporary command, while there is no > operator
+                if args.len() < 2 {
+                    eprintln!("Usage: write <file> <contents>");
+                    continue;
+                }
+                let file_name = args[0];
+                let contents = args[1..].join(" ");
+                std::fs::write(file_name, contents).unwrap();
+            }
+            _ => match execute(command, &args) {
+                Err(e) => eprintln!("Error executing command: {:?}", e),
+                _ => (),
+            },
         }
         println!("")
     }
