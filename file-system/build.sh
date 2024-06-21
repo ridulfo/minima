c043b8eaa074c9600aef6f017f6325f6d468bf9b
Nicolo Ridulfo <nicoloridulfo@gmail.com>
2024-06-21 09:47:05 +0200

    feat: use disk image instead of initramfs

--- file-system/build.sh
+++ file-system/build.sh
@@ -4,8 +4,28 @@ set -o errexit
 # The current directory is expected to be the project's root directory
 # TODO: make it less dependant on the current directory
 
+# Copy the file-system to the build directory
 cp -r file-system/rootfs build/rootfs
+
+# Check that the userland binaries are built
+if [ ! -f build/userland/init ]; then
+    echo "Userland binaries are not built."
+fi
+
+# Copy the userland binaries to the rootfs directory
 cp -p build/userland/init build/rootfs
 cp -p build/userland/* build/rootfs/bin
-cd build/rootfs
-find . | cpio -ov --format=newc | gzip -9 > ../rootfs.gz
+
+cd build
+
+# Create disk image file
+dd if=/dev/zero of=disk.img bs=1M count=100
+
+# Make a ext2 filesystem
+mkfs.ext2 disk.img
+
+# Create all of the directories
+find rootfs -type d | while read -r dir; do debugfs -w -R "mkdir ${dir#rootfs}" disk.img; done
+
+# Copy all of the files
+find rootfs -type f | while read -r file; do debugfs -w -R "write $file ${file#rootfs}" disk.img; done
--- makefile
+++ makefile
@@ -14,19 +14,19 @@ build/userland: $(wildcard userland/**/*)
 rootfs: build/rootfs.gz
 build/rootfs.gz: $(wildcard file-system/*) build/userland
 	./file-system/build.sh
+
 # Temporary fix to avoid building the kernel
 ifeq ($(MAKEKERN),0)
 run: userland rootfs
 else
 run: kernel userland rootfs
 endif
-	cd build;\
-	qemu-system-aarch64 -machine virt -cpu cortex-a57 -kernel Image -initrd rootfs.gz -nographic
+	qemu-system-aarch64 -machine virt -cpu cortex-a57 -m 1024 -kernel build/Image -drive file=build/disk.img,format=raw,if=none,id=hd0 -device virtio-blk-device,drive=hd0 -append "console=ttyAMA0 root=/dev/vda rw" -nographic
 
 clean:
-	rm -r build/rootfs build/userland build/rootfs.gz
+	rm -r build/rootfs build/userland build/disk.img
 
 clean-all:
-	rm -r build/rootfs build/userland build/rootfs.gz
+	rm -r build/rootfs build/userland build/disk.img
 	rm build/Image
 
